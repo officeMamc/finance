f9693ea5d05b45ac292d79e802b570f5feb6a9c6
officeMamc <dev.mamc.office@gmail.com>
2024-04-11 19:41:41 +0530

    made changes to learn

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,5 +9,25 @@ def hi():
 @app.route("/create-account")
 def crt_act():
     pass
+
+@app.route("/search") 
+def search():
+    list = ["Vrushab", 'VCH', "KCH", "NCH"]
+    query = request.args.get("q")
+    if query in list:
+        return render_template("search.html", query=query)
+    return render_template("search.html")
+
+@app.route("/search-resolve") 
+def search_resolve():
+    list = ["Vrushab", 'VCH', "KCH", "NCH"]
+    query = request.args.get("q")
+    if query in list:
+        return query
+    else:
+        return "0"
+
+
+
 
 app.run(host="0.0.0.0", port=5000, debug=True)
